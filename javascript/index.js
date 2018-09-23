778f8b524fd387da058a2adfb622edd30c9e4303
Floyd Jones <floydtjones@gmail.com>
2018-09-23 16:53:29 -0500

    Add iframe for dota 2 lfg

--- javascript/index.js
+++ javascript/index.js
@@ -26,6 +26,14 @@ function onMouseOver() {
   }
 }
 
-function onMouseExit() {
+$('.live').click(() => {
+  console.log('live');
+  $('iframe').removeClass('iframeHidden').addClass('iframeLive');
+  $('.returnButton').removeClass('returnHidden').addClass('returnLive');
+})
 
-}
+$('.returnButton').click(() => {
+  console.log('return');
+  $('iframe').removeClass('iframeLive').addClass('iframeHidden');
+  $('.returnButton').removeClass('returnLive').addClass('returnHidden');
+})
